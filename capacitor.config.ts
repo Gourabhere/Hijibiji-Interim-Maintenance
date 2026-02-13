b7394a2e80aab8b8b71ec0ce00cf802184d4fb49
Gourabhere <gourabhere@gmail.com>
2026-02-13 12:18:06 +0530

    feat: Initialize Android project with release build configuration, rename application, and add core assets.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.hijibiji.maintenance',
-  appName: 'HijibijiMaintenancePortal',
+  appName: 'Hijibiji',
   webDir: 'dist'
 };
 
